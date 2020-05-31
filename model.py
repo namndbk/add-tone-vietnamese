3a6f51e9b154475db755a410bee7c666394a260c
Nguyen Danh Nam <namnd2802@gmail.com>
2020-06-01 02:11:23 +0700

    Refactor add tone function.

--- model.py
+++ model.py
@@ -1,30 +1,18 @@
 from keras.models import model_from_json
 from character import CharacterModel
 from process import gen_ngram_from_text
+from preprocess import remove_accent
 import numpy as np
 from collections import Counter
-from preprocess import remove_accent
-import config
 
+import config
 import pickle
 import re
 import string
-
 import logging
+import sys
 
 logging.getLogger("tensorflow").setLevel(logging.WARNING)
-
-
-def extract_phrase(text):
-    punct = []
-    phrases = []
-    index = 0
-    for i, c in enumerate(text):
-        if c in string.punctuation:
-            phrases.append(text[index: i])
-            index = i + 1
-            punct.append(c)
-    return phrases, punct
 
 
 class ToneModel(object):
@@ -37,8 +25,13 @@ class ToneModel(object):
             self.models = model_from_json(model_json)
             self.models.load_weights(weights_file)
         except Exception as e:
-            raise e
-        index_alphabet = pickle.load(open(alphabet_file, "rb"))
+            assert e
+            sys.exit(1)
+        try:
+            index_alphabet = pickle.load(open(alphabet_file, "rb"))
+        except Exception as e:
+            assert e
+            sys.exit(1)
         self.codec = CharacterModel(index_alphabet=index_alphabet, maxlen=self.maxlen)
 
     def add_tone_ngram(self, ngram):
@@ -58,8 +51,9 @@ class ToneModel(object):
                 candidates[nid + wid].update([word])
         try:
             output = " ".join(c.most_common(1)[0][0] for c in candidates)
-        except:
+        except Exception as e:
             output = ""
+            assert e
         return output
 
     def add_tone_v2(self, sentence):
@@ -88,17 +82,20 @@ class ToneModel(object):
                         output += " "
                     if phrase[-1] == " ":
                         flag = True
-                    phrase = phrase.strip()
-                    for t1, t2 in zip(phrase.split(), out.strip().split()):
-                        if t1.lower() == t2.lower():
-                            y_pred += 1
-                        y_true += 1
+                    phrase = phrase.strip().split()
+                    out = out.strip().split()
                     try:
-                        for j, k in enumerate(phrase):
-                            if k.isupper():
-                                output += out[j].upper()
-                            else:
-                                output += out[j]
+                        for token_1, token_2 in zip(phrase, out):
+                            for j, k in enumerate(token_1):
+                                if k.isupper():
+                                    output += token_2[j].upper()
+                                else:
+                                    output += token_2[j]
+                            if token_1.lower() == token_2.lower():
+                                y_pred += 1
+                            y_true += 1
+                            output += " "
+                        output = output.strip()
                     except Exception as e:
                         print("\tInput: " + phrase)
                         print("\tOutput: " + out)
@@ -113,22 +110,6 @@ class ToneModel(object):
                 index = i + 1
         return y_pred, y_true, output
 
-    def add_tone(self, sentence):
-        list_phrase, punct = extract_phrase(sentence)
-        output = ""
-        for i, phrase in enumerate(list_phrase):
-            if len(phrase.split()) < 2:
-                output += phrase + punct[i] + " "
-            else:
-                out = self.add_tone_phrase(phrase.lower()).strip()
-                for j, c in enumerate(phrase.strip()):
-                    if c.isupper():
-                        output += out[j].upper()
-                    else:
-                        output += out[j]
-                output += punct[i] + " "
-        print(output)
-
 
 text = """Thí sinh chỉ được điều chỉnh đăng ký xét tuyển một lần và chỉ được sử dụng một trong hai phương thức trực \
 tuyến hoặc bằng phiếu. Với điều chỉnh bằng phương thức trực tuyến, các em sử dụng tài khoản và mật khẩu cá nhân đã \
@@ -136,9 +117,10 @@ tuyến hoặc bằng phiếu. Với điều chỉnh bằng phương thức tr�
 đầu. """
 text2 = """Ninh Dương Lan Ngọc sinh ngày 4/4/1990 ở TP HCM. Năm 2010, cô được biết đến lần đầu qua bộ phim Cánh đồng\
  bất tận. Nhờ gương mặt sáng, diễn xuất tự nhiên, cô được ví là "ngọc nữ" khi bước vào làng giải trí Việt."""
+text3 = """Bộ trưởng Quốc phòng Chetta Thanajaro khẳng định"""
 if __name__ == "__main__":
     model_file = "models/modelv2/model_v2.json"
     weights_file = "models/modelv2/best_model_v2.hdf5"
     alphabet_file = "idxabc.pickle"
     model = ToneModel(config, model_file, weights_file, alphabet_file)
-    print(model.add_tone_v2("""thiếu tướng Mowaffaq Mohammed Dahham"""))
+    print(model.add_tone_v2(text2))
